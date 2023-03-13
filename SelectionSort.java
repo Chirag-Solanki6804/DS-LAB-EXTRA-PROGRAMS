ffe76335b954f264bbe4577dca7d228b919e6080
Chirag Solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2023-03-13 19:53:05 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr={7,8,3,1,4};
+
+        for (int i=0;i<arr.length-1;i++){
+            int smallestIndex=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[smallestIndex] >arr[j]){
+                    smallestIndex=j;
+                }
+            }
+            int temp=arr[smallestIndex];
+            arr[smallestIndex]=arr[i];
+            arr[i]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
